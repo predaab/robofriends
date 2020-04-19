a5249609e15d2f2bd916b68265680ea63bee1f16
predaab <63191446+predaab@users.noreply.github.com>
2020-04-19 15:01:10 +0300

    Updates

---
+++ precache-manifest.b667319ee8ff030ff12bf0eb9e4af289.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "375c4fb46b070d40537d0c5376e6429c",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "a76249b7330f2fa9434c",
+    "url": "/robofriends/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "96ce7aaa6089395b8be2",
+    "url": "/robofriends/static/css/main.2682a660.chunk.css"
+  },
+  {
+    "revision": "a76249b7330f2fa9434c",
+    "url": "/robofriends/static/js/2.2f7a48c9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.2f7a48c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96ce7aaa6089395b8be2",
+    "url": "/robofriends/static/js/main.91552df1.chunk.js"
+  },
+  {
+    "revision": "8adbf24e3312ffc2ae3e",
+    "url": "/robofriends/static/js/runtime-main.403370f6.js"
+  },
+  {
+    "revision": "dfb87c63e75935acbd79937ed6188c19",
+    "url": "/robofriends/static/media/Vieytes.dfb87c63.woff"
+  }
+]);
